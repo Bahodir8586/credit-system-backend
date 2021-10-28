4cd5cbd3702049d355efd71e241589ba2bdd4f60
Bahodir8586 <bahodira213@gmail.com>
2021-10-28 22:33:02 +0500

    db connection and project setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch((e) => {
+    console.log(e);
+    console.log('ERROR');
+  });
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
