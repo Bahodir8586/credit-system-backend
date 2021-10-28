328de5ff85256234841cc1914d2a077e7486f40e
Bahodir8586 <bahodira213@gmail.com>
2021-10-28 22:41:42 +0500

    body-parsre

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const hpp = require('hpp');
+const helmet = require('helmet');
 const rateLimiter = require('express-rate-limit');
 
 const app = express();
-const limiter = rateLimit({
+app.use(helmet());
+app.use(bodyParser.json());
+const limiter = rateLimiter({
   max: 100,
   windowMs: 60 * 1000,
   message: 'Too many requests from this IP',
