6f2ec6aed95af1a86b47374545e14aa4152bf90b
Bahodir8586 <bahodira213@gmail.com>
2021-10-28 22:36:46 +0500

    rate limiting

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const hpp = require('hpp');
+const rateLimiter = require('express-rate-limit');
 
 const app = express();
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 1000,
+  message: 'Too many requests from this IP',
+});
 app.use(hpp());
+app.use('/api', limiter);
 module.exports = app;
