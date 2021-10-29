d525ff040bd49be374864b9c67d72a00bbbfc39e
Bahodir8586 <bahodira213@gmail.com>
2021-10-29 11:08:10 +0500

    product model

---
+++ models/productModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name can not be empty'],
+  },
+  mainImage: {
+    type: String,
+    required: [true, 'Please provide image of the product'],
+  },
+  images: [String],
+  amount: {
+    type: Number,
+    min: [0, 'Amount of the product can not be negative'],
+  },
+  rating: {
+    type: Number,
+    default: 5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+    set: (val) => Math.round(val * 10) / 10,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please provide price of the product'],
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price',
+    },
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
