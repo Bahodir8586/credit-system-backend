7859b700b73b83b1fcf64654f945cfc935b27eba
Bahodir8586 <bahodira213@gmail.com>
2021-11-07 15:27:00 +0500

    connecting email to controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -136,10 +136,23 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const token = await user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    status: 'Success',
-    token,
-  });
+  try {
+    const passwordResetUrl = `http://localhost:3000/resetPassword/${token}`;
+    await new Email(user, passwordResetUrl).sendPasswordReset();
+
+    res.status(200).json({
+      status: 'Success',
+      message: 'Token sent to email',
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later', 500)
+    );
+  }
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
