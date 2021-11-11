970aa0c532ad9da0a61f00af8fbec7d2dce011bb
Bahodir8586 <bahodira213@gmail.com>
2021-11-11 22:01:42 +0500

    delete

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require('xss-clean');
 
 const userRouter = require('./routes/userRoutes');
 const authRouter = require('./routes/authRoutes');
+const productRouter = require('./routes/productRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
@@ -34,6 +35,7 @@ app.use('/api', limiter);
 
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/products', productRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
--- controllers/productController.js
+++ controllers/productController.js
@@ -32,4 +32,13 @@ exports.getProduct = catchAsync(async (req, res, next) => {
 
 exports.createProduct = catchAsync(async (req, res, next) => {});
 
-exports.deleteProduct = catchAsync(async (req, res, next) => {});
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
