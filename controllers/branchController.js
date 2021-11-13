03f037c56a8b886857fc5d5814dc8d066d89fc44
Bahodir8586 <bahodira213@gmail.com>
2021-11-13 08:13:00 +0500

    branch crud basics

--- controllers/branchController.js
+++ controllers/branchController.js
@@ -4,3 +4,45 @@ const sharp = require('sharp');
 const Branch = require('../models/branchModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, callback) => {
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true);
+  } else {
+    callback(
+      new AppError('Not an image! Please upload only images.', 400),
+      false
+    );
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadBranchImages = upload.single('image');
+
+exports.resizeBranchImage = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  // 1) Cover image
+  req.body.image = `branch-${Date.now()}.jpeg`;
+  try {
+    await sharp(req.file.buffer)
+      .resize(640, 640)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/branches/${req.body.image}`);
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log(req.body.image);
+  next();
+});
+
+exports.createBranch = catchAsync(async (req, res, next) => {});
+exports.getAllBranches = catchAsync(async (req, res, next) => {});
+exports.getSingleBranch = catchAsync(async (req, res, next) => {});
+exports.updateBranch = catchAsync(async (req, res, next) => {});
+exports.deleteBranch = catchAsync(async (req, res, next) => {});
