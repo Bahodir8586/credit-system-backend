2dc3c663fe4a2fccf4b86c5ff76f2d767d3b75ad
Bahodir8586 <bahodira213@gmail.com>
2021-11-30 18:13:29 +0500

    branch routes

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const authController = require('../controllers/authController');
+const branchController = require('../controllers/branchController');
 
 const router = express.Router();
+
+router.get('/', branchController.getAllBranches);
+router.get('/:id', branchController.getSingleBranch);
+
+router.use(authController.protect, authController.restrictTo('admin'));
+
+router.route('/').post(branchController.createBranch);
+router
+  .route('/:id')
+  .patch(branchController.updateBranch)
+  .delete(branchController.deleteBranch);
 
 module.exports = router;
